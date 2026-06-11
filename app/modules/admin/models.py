676f1eb16227024a82afc83eb532fb0634be1103
PressureDraper <warmarines00@hotmail.com>
2026-06-11 14:22:45 -0600

    fix(admin): remove useless table fields and add seeds

--- app/modules/admin/models.py
+++ app/modules/admin/models.py
@@ -8,8 +8,6 @@ class Settings(Base, IDMixin, TimestampMixin):
     __tablename__ = "site_settings"
 
     site_name: Mapped[str] = mapped_column(String(100), nullable=False)
-    site_logo_url: Mapped[str] = mapped_column(String(255), nullable=False)
-    favicon_url: Mapped[str | None] = mapped_column(String(255))
 
     login_img_url: Mapped[str] = mapped_column(String(255), nullable=False)
     
